0d646a30e954f93cd0786c4b31e6be9cf2ec7c23
Ludmila Silva <lud.carina@gmail.com>
2023-09-30 19:19:01 -0300

    Repository upload

---
+++ assets/js/poke-api.js
@@ -0,0 +1,44 @@
+const pokeApi = {}
+
+function convertPokeApiDetailToPokemon(pokeDetail){
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.id
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+    //buscar lista de pokemons no servidor
+    //Converte response htpp para json e retorna uma promise
+    //Recebe body convertido
+    //Encadeamento de then
+    //Arrow function => sintaxe reduzida
+    //Lista transformada em nova lista de promessas de detalhe do pokemon (nova requisição) em json
+    //Quando todas as requisições terminarem, retorna uma lista completa de detalhes
+
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
+
+}
---
+++ assets/js/pokemon-model.js
@@ -0,0 +1,12 @@
+class Pokemon {
+    number;
+    name;
+    SVGUnitTypes;
+    types = [];
+    photo;
+    stats = [];
+    abilities = [];
+    species = [];
+    height;
+    weight;
+}
